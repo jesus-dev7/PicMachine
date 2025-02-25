ba5f82f26ef6ae86352ab07f3177220c04f592fe
jesus <kids.anguita@gmail.com>
2025-02-25 10:00:24 +0100

    Chat fonctionnel.

--- static/JS.js
+++ static/JS.js
@@ -1,26 +1,40 @@
 document.querySelector('.btnIm').addEventListener('click', function() {
+    
     var NewImage = document.getElementById("NewImage");
+
     NewImage.src = "/img_get?" + new Date().getTime();  // Force le rechargement
+
 });
 
-
 document.querySelector('#BtnChat').addEventListener('click', function() {
+
     window.location.href = "/Chat";  // Redirige vers la page Chat
+
 });
 
 
 // Récupérer et afficher les IPs
+
 async function IPs() {
+
     const reponse = await fetch("/recup_IPs");
+
     const data = await reponse.json();
+
     const ListIPs = document.getElementById("IPS");
 
     if (data.IPs && data.IPs.length > 0) {
+
         ListIPs.innerHTML = data.IPs.join("<br>");
+
     } else {
+
         ListIPs.innerHTML = "Pas d'IP.";
+
     }
+
 }
 
 IPs();
+
 setInterval(IPs, 5000);
